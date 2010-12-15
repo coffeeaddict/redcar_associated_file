ae0dca300a51e66184c35158f63bf2295d71e25d
Hartog de Mik <hartog.de.mik@simplic.it>
2010-12-15 22:01:00 +0100

    We can open a controller from a view now

--- lib/associated_file.rb
+++ lib/associated_file.rb
@@ -37,6 +37,8 @@ module Redcar
     end
   
     class AssociatedCommand < EditTabCommand
+      attr_accessor :file_path, :project_root
+      
       # check for remote project and display a dialog if so
       def remote_project?
         # just assuming that this is true
@@ -48,22 +50,64 @@ module Redcar
         return false
       end
       
+      def goto_definition file, definition
+        Project::Manager.open_file file
+        re = Regexp.new(Regexp.escape(definition))
+        
+        DocumentSearch::FindNextRegex.new(re,true).run_in_focussed_tab_edit_view
+      end
+      
       def log message
         puts "-=-> AF: #{message}"
       end
     end
     
+    # Open the related (rails) view when in controller and vice versa
     class OpenViewControllerCommand < AssociatedCommand
+      
       def execute
         return if remote_project?
         
-        log doc.inspect
-      end  
+        root = Project::Manager.focussed_project.path
+        # first, lets see if the project root is a rails root - else do nothing
+        unless (
+          File.exists?(File.join(root, "config", "environment.rb")) and
+          File.exists?(File.join(root, "config", "boot.rb"))
+        )
+          log "You don't seem to be in a rails root"
+          return
+        end
+        
+        self.project_root = root
+        self.file_path    = doc.path.gsub!(root, "")
+        
+        # if the doc is a controller, show the view and vice versa
+        if file_path =~ /controllers\/\w+_controller.rb/
+          switch_to_view
+        elsif file_path =~ /\/views\//
+          switch_to_controller
+        end
+      end
+      
+      def switch_to_view
+      end
+      
+      # open the controller 
+      def switch_to_controller
+        unless ( match = self.file_path.match(/\/views\/([^\/]+)/) )
+          return
+        end
+        action     = File.basename(self.file_path).split(".").first
+        name       = match[1]
+        controller = File.join(self.project_root, "/app/controllers/#{name}_controller.rb")
+        
+        definition = "def #{action}"
+        
+        goto_definition(controller, definition)
+      end
     end
     
-    class OpenControllerCommand < Command; end
-    class OpenTestCommand < Command; end
-    class OpenClassCommand < Command; end
-    class OpenFixtureCommand < Command; end
+    class OpenTestClassCommand < AssociatedCommand; end    
+    class OpenTestFixtureCommand < AssociatedCommand; end
   end
 end
