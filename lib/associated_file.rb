03026ee4571022abf07dfaae3519fda73a923d8f
Hartog C. de Mik <hartog.de.mik@gmail.com>
2010-12-16 23:18:15 +0100

    Vendored in active_support. Yay!

--- lib/associated_file.rb
+++ lib/associated_file.rb
@@ -1,3 +1,25 @@
+# vendor in active support
+$: << File.expand_path('../vendor/activesupport-3.0.3/lib', __FILE__)
+
+require 'active_support'
+require 'active_support/inflector/inflections'
+require 'active_support/inflections'
+
+require 'associated_file/association_matcher'
+require 'associated_file/associated_command'
+require 'associated_file/open_view_controller_command'
+require 'associated_file/open_test_class_command'
+require 'associated_file/open_test_fixture_command'
+
+class String
+  def singularize
+    ActiveSupport::Inflector.singularize self
+  end
+  def pluralize
+    ActiveSupport::Inflector.pluralize self
+  end
+end
+
 module Redcar
   
   # load associated file (eg spec or test or (rails) view/controller) with a 
