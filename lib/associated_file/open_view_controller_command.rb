9ea89cd91e3238d2c3e9096a6888a3d466e99fb0
syzspectroom <syzspectroom@gmail.com>
2011-01-18 13:58:24 +0200

    added html.haml view support

--- lib/associated_file/open_view_controller_command.rb
+++ lib/associated_file/open_view_controller_command.rb
@@ -1,6 +1,6 @@
 module Redcar
   class AssociatedFile
-    
+
     # Open the related (rails) files:
     # * controller[action] <--> view
     # * model              ---> controller
@@ -8,12 +8,12 @@ module Redcar
     class OpenViewControllerCommand < AssociatedCommand
       def execute
         return if remote_project?
-        
+
         self.project_root = Project::Manager.focussed_project.path
         return unless rails_project?
-        
+
         self.file_path    = doc.path.gsub(project_root, "")
-        
+
         # if the doc is a controller, show the view and vice versa
         if path_matcher.is_controller?
           switch_to_view
@@ -21,7 +21,7 @@ module Redcar
           switch_to_controller
         end
       end
-      
+
       # open the view from the controller
       def switch_to_view
         unless ( name = path_matcher.controller_name )
@@ -30,26 +30,26 @@ module Redcar
         # figure out what the name of def we are in is
         action = find_function_under_cursor
         view   = File.join(project_root, "/app/views/#{name}/#{action}")
-        
+
         # open the according view (most likely non existant)
         if !File.exists?(view)
-          %w(erb html.erb haml js.erb xml.erb).each do |try|
+          %w(erb html.erb haml html.haml js.erb xml.erb).each do |try|
             if File.exists?("#{view}.#{try}")
               view += ".#{try}"
               break
             end
           end
         end
-        
+
         open_file view
       end
-      
+
       # open the controller from the view or model
       def switch_to_controller
         unless ( name = (path_matcher.view_name || path_matcher.model_name) )
           return
         end
-        
+
         action = nil
       	if path_matcher.is_view?
       	  action = File.basename(file_path).split(".").first
@@ -58,7 +58,7 @@ module Redcar
         end
 
         controller = "/app/controllers/#{name}_controller.rb"
-        
+
         if action
           goto_definition controller, "def #{action}"
         else
@@ -68,3 +68,4 @@ module Redcar
     end
   end
 end
+
